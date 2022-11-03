716e63506b84d2ebf4d30a137db65593ff38648c
QossayZeineddin <103140839+QossayZeineddin@users.noreply.github.com>
2022-11-03 10:41:04 +0200

    .

---
+++ BulkyBook/Areas/Customer/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BulkyBook.Areas.Admin.Models;
+
+namespace BulkyBook.Areas.Customer.Models
+{
+
+    public class ShoppingCart
+    {
+        public Product Product { get; set; }
+        [Range(1,1000 , ErrorMessage = "please enter a number btween 1 and 1000")]
+        public  int count { get; set; }
+
+    }
+}
